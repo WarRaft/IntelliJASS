a2f50cfce80d8f73c32a7db359f316ecb7b0f520
nazarpunk <nazarpunk@gmail.com>
2023-11-09 01:46:14 +0200

    JASS->JGAS

--- src/main/java/guru/xgm/jgas/psi/PsiReferenceContributor_vJGAS.java
+++ src/main/java/guru/xgm/jgas/psi/PsiReferenceContributor_vJGAS.java
@@ -1,4 +1,4 @@
-package guru.xgm.jass.psi;
+package guru.xgm.jgas.psi;
 
 import com.intellij.openapi.util.TextRange;
 import com.intellij.patterns.PlatformPatterns;
@@ -6,10 +6,10 @@ import com.intellij.psi.*;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 
-import static guru.xgm.jass.lang.annotation.Annotator_JASS.SIMPLE_PREFIX_STR;
-import static guru.xgm.jass.lang.annotation.Annotator_JASS.SIMPLE_SEPARATOR_STR;
+import static guru.xgm.jgas.lang.annotation.Annotator_JGAS.SIMPLE_PREFIX_STR;
+import static guru.xgm.jgas.lang.annotation.Annotator_JGAS.SIMPLE_SEPARATOR_STR;
 
-final class PsiReferenceContributor_JASS extends PsiReferenceContributor {
+final class PsiReferenceContributor_vJGAS extends PsiReferenceContributor {
     @Override
     public void registerReferenceProviders(@NotNull PsiReferenceRegistrar registrar) {
         registrar.registerReferenceProvider(PlatformPatterns.psiElement(PsiLiteralExpression.class),
@@ -24,7 +24,7 @@ final class PsiReferenceContributor_JASS extends PsiReferenceContributor {
 
                         if ((value != null && value.startsWith(SIMPLE_PREFIX_STR + SIMPLE_SEPARATOR_STR))) {
                             TextRange property = new TextRange(SIMPLE_PREFIX_STR.length() + SIMPLE_SEPARATOR_STR.length() + 1, value.length() + 1);
-                            return new PsiReference[]{new PsiReferenceBase_JASS(element, property)};
+                            return new PsiReference[]{new PsiReferenceBase_JGAS(element, property)};
                         }
                         return PsiReference.EMPTY_ARRAY;
                     }
