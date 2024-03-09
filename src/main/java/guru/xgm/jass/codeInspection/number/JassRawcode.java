c90704106297765578bdd5a34b2bf40fa8dacbdc
nazarpunk <nazarpunk@gmail.com>
2024-03-09 17:09:57 +0200

    fix rawcode 1
    Took 42 minutes

--- src/main/java/guru/xgm/jass/codeInspection/number/JassRawcode.java
+++ src/main/java/guru/xgm/jass/codeInspection/number/JassRawcode.java
@@ -1,11 +1,11 @@
-package guru.xgm.jass.codeInspection.rawval;
+package guru.xgm.jass.codeInspection.number;
 
 import com.intellij.psi.PsiElement;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
-public class JassRawVal {
+public class JassRawcode {
 
     static int bytes2int(byte[] bytes) {
         int i = 0;
@@ -15,7 +15,7 @@ public class JassRawVal {
         return i;
     }
 
-    JassRawVal(PsiElement elem) {
+    JassRawcode(PsiElement elem) {
         final var text = elem.getText();
         final var textbytes = text.getBytes(StandardCharsets.ISO_8859_1);
 
@@ -24,14 +24,18 @@ public class JassRawVal {
             safe = false;
             bytes = new byte[0];
             integer = 0;
+            intstr = "0";
             hex = "0x0";
+            validLength = false;
             return;
         }
         error = false;
         bytes = Arrays.copyOfRange(textbytes, 1, textbytes.length - 1);
         safe = isSafe();
 
+        validLength = bytes.length == 1 || bytes.length == 4;
         integer = bytes2int(bytes);
+        intstr = Long.toString(integer & 0xFFFFFFFFL);
         hex = "0x" + String.format("%02x", integer);
     }
 
@@ -43,8 +47,10 @@ public class JassRawVal {
     }
 
     final boolean safe;
+    final boolean validLength;
     final boolean error;
     final byte[] bytes;
     final int integer;
+    final String intstr;
     final String hex;
 }
