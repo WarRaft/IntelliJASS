1c45851bf871571e57fa6437d9e32a2dbe0a5850
nazarpunk <nazarpunk@gmail.com>
2024-07-01 01:44:29 +0300

    jass reference prepare

--- src/main/kotlin/raft/war/plugin/openapi/startup/PluginProjectActivity.kt
+++ src/main/kotlin/raft/war/plugin/openapi/startup/PluginProjectActivity.kt
@@ -1,21 +1,34 @@
 package raft.war.plugin.openapi.startup
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
-import com.intellij.openapi.vfs.VirtualFileManager
-import java.nio.file.Paths
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.psi.PsiManager
+import com.intellij.util.PathUtil
+import com.intellij.util.io.URLUtil
+import raft.war.language.jass.lang.JassLanguage
+import java.io.File
 
 class PluginProjectActivity : ProjectActivity {
     override suspend fun execute(project: Project) {
-        print("catcha!\n")
-        val resourceUrl = this::class.java.classLoader.getResource("/resources/sdk/common.j")
-        print(resourceUrl)
-        resourceUrl?.let {
-            val filePath = Paths.get(it.toURI()).toString()
-            print(filePath)
+        val jarPath = PathUtil.getJarPathForClass(PluginProjectActivity::class.java)
+        val dir = if (jarPath.endsWith(".jar")) {
+            VfsUtil.findFileByURL(URLUtil.getJarEntryURL(File(jarPath), "sdk"))
+        } else
+            VfsUtil.findFileByIoFile(File("$jarPath/sdk"), true)
 
-            val v = VirtualFileManager.getInstance().findFileByUrl("file://$filePath")
-            print(v)
+        val application = ApplicationManager.getApplication()
+        val psiManager = PsiManager.getInstance(project)
+
+        application.runReadAction {
+            //val set = mutableSetOf<VirtualFile>()
+            dir?.children?.forEach {
+                val psiFile = psiManager.findFile(it)
+                if (psiFile != null) {
+                    if (psiFile.language !is JassLanguage) return@forEach
+                }
+            }
         }
     }
 }
